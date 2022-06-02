965922f108c9a0fc07178f57cf5e46cfae2344af
Sai Krishna Srinivas Ganduri <sganduri@twilio.com>
2022-06-02 15:57:40 +0530

    renoved a variable

--- variables.tf
+++ variables.tf
@@ -3,9 +3,3 @@ variable "GITHUB_PERSONAL_TOKEN" {
   description = "Github personal access token"
   default = ""
 }
-
-variable "bucket_name" {
-    type = string
-    description = "Name of the S3 bucket"
-    default = "poc-codebuild-artifact"
-}
